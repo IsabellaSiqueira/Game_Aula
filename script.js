3ccadeb1dc334116e5793ab145903e783005d200
ÏsabellaSiqueira <isabella.tragante@hotmail.com>
2025-10-23 08:25:47 -0300

    Adicionando js  básico

---
+++ script.js
@@ -0,0 +1,4 @@
+const game = document.querySelector('.game');
+const bird = document.querySelector('.bird');
+const score = document.querySelector('.score');
+const messege = document.querySelector('.messege');
