004f913c324097bf58b1b1c7a23088776b2629af
ÏsabellaSiqueira <isabella.tragante@hotmail.com>
2025-10-23 08:54:02 -0300

    feat: Implementa lógica para iniciar o jogo com a tecla Enter

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 const game = document.querySelector('.game');
 const bird = document.querySelector('.bird');
-const score = document.querySelector('.score');
-const messege = document.querySelector('.messege');
+const hudScore = document.querySelector('.score');
+const message = document.querySelector('.message');
+const btnStart = document.querySelector('.btnStart');
+
+const State = {
+    start: "START",
+    play: "PLAY",
+    end: "END"
+};
+
+let state = State.start;
+
+let score = 0;
+let bestScore = 0; // TODO: Adicionar Lógica.
+
+// btnStart.addEventListener('click', () => {
+//     state = State.play;
+//     message.innerHTML = 'Em Jogo'
+// });
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == "Enter"){
+        state = State.play;
+        console.log("No estado" + state);
+        message.innerHTML = 'Voa, Voa, Voa!';
+    }
+});
