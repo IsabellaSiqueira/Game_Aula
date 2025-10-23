af5755d8a43e3b1b51c2159247188bb7beaca3a6
ÏsabellaSiqueira <isabella.tragante@hotmail.com>
2025-10-23 09:32:44 -0300

    adicionando os frames e a função game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const game = document.querySelector('.game');
+// const game = document.querySelector('.game');
 const bird = document.querySelector('.bird');
 const hudScore = document.querySelector('.score');
 const message = document.querySelector('.message');
@@ -15,6 +15,8 @@ let state = State.start;
 let score = 0;
 let bestScore = 0; // TODO: Adicionar Lógica.
 
+let frames = 0; // TODO: Verificar se necessita colocar no startGame.
+
 // btnStart.addEventListener('click', () => {
 //     state = State.play;
 //     message.innerHTML = 'Em Jogo'
@@ -28,7 +30,7 @@ function startGame() {
             message.innerHTML = 'Voa, Voa, Voa!';
         }
     })
-    
+
     function resetPositionBird() {
         bird.style.top = '40vh';
         bird.style.left = '30vw';
@@ -40,6 +42,30 @@ function startGame() {
 
     resetPositionBird();
     resetScore();
+};
+
+function game() {
+    // state = State.play;
+
+    function loop() {
+        update();
+        render();
+        requestAnimationFrame(loop);
+    };
+
+    requestAnimationFrame(loop);
+
+    function update() {
+        if (state === State.play) {
+            frames++;
+        };
+    };
+
+    function render() {
+        if (state === State.play) {
+            message.innerHTML = 'Loop rodando em' + frames;
+        }
+    }
 };
 
 startGame();
